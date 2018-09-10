8dc54b07e09de57a4c764a9b45c505b7fda20c0d
jiangdi <dijiang@borderxlab.com>
2018-09-10 14:53:57 +0800

    fix error

--- session1/request.rb
+++ session1/request.rb
@@ -21,7 +21,14 @@ class Tools
     uri = URI(str)
     res = Net::HTTP.new(uri.host, uri.port)
     code = res.head(uri.request_uri, headers).code.to_i
-    res.use_ssl = true
+    head = uri.scheme
+    puts head
+    #need open ssl
+    if head.equal?('https') {
+      res.use_ssl = true
+    }
+    end
+
     if code == 400 then
       res.get(uri.request_uri, headers) do |chunk|
         # json_object = JSON.parse(chunk)
@@ -30,7 +37,7 @@ class Tools
         # puts model
         # puts json_object
         file_manager = File.new('', 'r+')
-        file_manager.reopen(file_manager)
+        # file_manager.reopen(file_manager)
         file_manager.write(chunk)
         File.open(file_manager).each { |f|
 
@@ -41,9 +48,9 @@ class Tools
   end
 end
 
-#https://www.apiopen.top/journalismApi
 STDOUT.flush
 url = gets.chomp
 get_request_data = Tools.new
 result = get_request_data.test_url(url)
 puts result
+#https://www.apiopen.top/satinApi?type=1&page=1 for test
